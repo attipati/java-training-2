3a774afea7205c3fcc85d016a38828acb59f6e26
smuppirishet <96856460+swethamuppirishetty@users.noreply.github.com>
2022-01-06 23:14:29 +0530

    JAVA-TRAINING

---
+++ Collections.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import ListDemo.SetDemo;
+
+
+
+
+
+class Collections{
+    public static void main(String[] args) {
+        
+       ListDemo ld=new ListDemo();
+         ld.printList();
+         ld.find(10);
+        
+         
+        
+    }
+}
+class ListDemo{
+    List<Integer> numbers;
+    public ListDemo(){
+        numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(3);
+        numbers.add(4);
+
+    }
+    public void printList(){
+        for(Integer i:numbers){
+            System.out.println(i);
+        }
+    }
+    public void find(Integer i){
+        if(numbers.contains(i)){
+            System.out.printf("found %d at lacation:%d %n",i,numbers.indexOf(i));
+
+        }
+        else{
+            System.out.printf("%d not found",i);
+        }
+    }
+
+
+
+
+
+
+
+    class SetDemo{
+        List<Integer> numbers;
+        public SetDemo(){
+            numbers = new HashSet<>();
+            numbers.add(1);
+            numbers.add(2);
+            numbers.add(3);
+            numbers.add(3);
+            numbers.add(4);
+    
+        }
+        public void printSet(){
+            for(Integer i:numbers){
+                System.out.println(i);
+            }
+        }
+    }
+}
